315c1a72f3a95d569467d483f3ca51a7e9eca9c1
calvince <calokongo@gmail.com>
2022-08-23 23:43:58 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+function myFunction() {
+    return "bob"
+}
+
+var customerName = "bob";
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+// upperCaseCustomerName() 'BOB'
+
+var setBestCustomer = "bob";
